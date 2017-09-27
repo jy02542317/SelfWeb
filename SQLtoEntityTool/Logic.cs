7c42e613d10c2a0b517ee9a437804d70d0a966dd
jy02542317 <811495111@qq.com>
2017-09-27 17:34:21 +0800

    111

---
+++ SQLtoEntityTool/Logic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Util;
+
+namespace SQLtoEntityTool
+{
+    public class Logic
+    {
+        public bool ConnectSQLTest(string connection)
+        {
+            SqlConnection cmd = new SqlConnection(connection);
+            try
+            {
+                cmd.Open();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                cmd.Close();
+            }
+        }
+
+        public string ComposeSQL(string ip, string db,string dbus, string dbpd)
+        {
+            return string.Format(@"Data Source={0};Initial Catalog={1};User ID={2};Password={3}",ip,db,dbus,dbpd);;
+        }
+
+    }
+}
